957c6300f93f645a521be8de30a5b233f8fdbf4b
wangyueyang <1632085751@qq.com>
2021-12-09 19:30:46 +0800

    feat: jscpd 包测试重复代码，通过  .jscpd.json 配置

---
+++ jscpd-demo/index.js
@@ -0,0 +1,43 @@
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+
+function a(x, y) {
+  return x + y;
+}
+
+function a(x, y) {
+  return x + y;
+}
+
+function a(x, y) {
+  return x + y;
+}
+
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
+function a(x, y) {
+  return x + y;
+}
